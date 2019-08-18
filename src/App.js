5273ca6d41e9c67dc8fa4a07899bbbae21cc0365
MrSparsh <mr.sparsh13@gmail.com>
2019-08-18 22:38:14 +0530

    To Do List App Created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+
+function ListItem(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        {props.listItemData}
+      </div>
+      <button
+        onClick={() => props.deleteItemClicked(props.index)}
+      >
+        Delete
+      </button>
     </div>
   );
 }
 
+function ListBox(props) {
+  return props.listItems.map((item,index) => (
+    <ListItem 
+      listItemData={item} 
+      index={index} 
+      deleteItemClicked={props.deleteItemClicked} 
+    />
+  ));
+}
+function AddItemBox(props) {
+  return (
+    <div>
+      <input
+        value={props.inputData}
+        key="newItemData"
+        onChange={props.inputDataChanged}
+      />
+      <button
+        onClick={() => {
+          props.addItemClicked(props.inputData);
+        }}
+      >
+        Add{" "}
+      </button>
+    </div>
+  );
+}
+
+function App() {
+  const [listItems, setListItems] = useState([]);
+  const [inputData, setInputData] = useState("");
+
+  const deleteItemClicked = indexToDelete => {
+    console.log(indexToDelete);
+    setListItems(listItems.filter((item,index) => {return index!==indexToDelete}));
+  };
+  const addItemClicked = itemData => {
+    console.log(itemData);
+    setListItems(listItems.concat(itemData));
+    setInputData("");
+  };
+  const inputDataChanged = event => {
+    console.log(event.target.value);
+    setInputData(event.target.value);
+  };
+  return (
+    <div>
+      <h1>To Do App</h1>
+      <AddItemBox
+        addItemClicked={addItemClicked}
+        inputData={inputData}
+        inputDataChanged={inputDataChanged}
+      />
+      <ListBox 
+        listItems={listItems} 
+        deleteItemClicked={deleteItemClicked} 
+      />
+    </div>
+  );
+}
 export default App;
